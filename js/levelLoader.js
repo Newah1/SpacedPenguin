a0186a25128f3eddd68ab1649236da0075d91ef0
Noah Doughton <nwpgaming@gmail.com>
2025-08-17 14:21:44 -0400

    Added level 16

--- js/levelLoader.js
+++ js/levelLoader.js
@@ -439,7 +439,7 @@ export class LevelLoader {
     
     async loadDefaultLevels() {
         // Load built-in level definitions
-        const totalLevels = 15;
+        const totalLevels = 16;
         for (let i = 1; i <= totalLevels; i++) {
             await this.tryLoadLevelFile(i, `levels/level${i}.json`);
         }
